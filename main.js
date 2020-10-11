7ee47e9d4c798b7b55ee5d44b7e68dc1bbff0f08
Roman Brunka <rbrunka@gmail.com>
2020-10-11 11:28:35 +0200

    simple changes

--- main.js
+++ main.js
@@ -4,46 +4,37 @@
  * Created with @iobroker/create-adapter v1.29.1
  */
 
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
+
 const utils = require('@iobroker/adapter-core');
+const axios = require('axios');
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
+class Defro extends utils.Adapter {
 
-class Template extends utils.Adapter {
-
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
     constructor(options) {
         super({
             ...options,
-            name: 'template',
+            name: 'windhager',
         });
+
         this.on('ready', this.onReady.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
-        // this.on('objectChange', this.onObjectChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
+        this.killTimeout = null;
     }
 
     /**
      * Is called when databases are connected and adapter received configuration.
      */
     async onReady() {
-        // Initialize your adapter here
+        const self = this;
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
+        const defroLogin = this.config.login;
+        const defroPass = this.config.pass;
+        const defroUserID = this.config.userID;
+        const defroToken = this.config.token;
 
-        /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
+        this.log.info('UserID: ' + defroUserID);
+        this.log.info('Token: ' + defroToken);
+
         await this.setObjectNotExistsAsync('testVariable', {
             type: 'state',
             common: {
@@ -56,33 +47,8 @@ class Template extends utils.Adapter {
             native: {},
         });
 
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
 
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
-
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
-
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
-
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+        this.killTimeout = setTimeout(this.stop.bind(this), 10000);
     }
 
     /**
@@ -155,14 +121,8 @@ class Template extends utils.Adapter {
 
 }
 
-// @ts-ignore parent is a valid property on module
 if (module.parent) {
-    // Export the constructor in compact mode
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
-    module.exports = (options) => new Template(options);
+    module.exports = (options) => new Defro(options);
 } else {
-    // otherwise start the instance directly
-    new Template();
+    new Defro();
 }
